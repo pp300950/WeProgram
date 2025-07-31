b961fbf3b1b7d4c1f7e7d3f691ba0b4bc0b4f41f
Phongsakorn PP <128141477+pp300950@users.noreply.github.com>
2025-07-31 07:09:06 +0000

    โครงสร้างโค้ด เพิ่มคอมเมนต์

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from utils import greet
-from math_tools import add
+from math_tools import (
+    CircleArea,
+    TriangleArea,
+    EquilateralTriangleArea,
+    IsoscelesTriangleArea,
+    ScaleneTriangleArea,
+    SquareArea,
+    RectangleArea,
+    PentagonArea,
+    HexagonArea,
+    add
+)
+
+CircleArea(10)
+TriangleArea(5, 4)
 
 print(greet("Gg"))
 print(add(2, 4))
--- math_tools.py
+++ math_tools.py
@@ -3,3 +3,42 @@ def add(a, b):
 
 def subtract(a, b):
     return a - b
+
+#หาพื้นที่ด้วย ชอลเลสฟอมูล่า
+#เเสดงผลกราฟ
+
+#พื้นที่วงกลม
+def CircleArea(radius):
+    return print("ไม่มีอะไร",radius)
+
+#พื้นที่สามเหลี่ยมทั่วไป
+def TriangleArea(base, height):
+    return print("ไม่มีอะไร",base, height)
+
+## พื้นที่สามเหลี่ยมด้านเท่า
+def EquilateralTriangleArea(side):
+    return print("ไม่มีอะไร",side)
+
+## พื้นที่สามเหลี่ยมน่าจั่ว 
+def IsoscelesTriangleArea(base, equalSide):
+    return print("ไม่มีอะไร",base, equalSide)
+
+## พื้นที่สามเหลี่ยมด้านไม่เท่า
+def ScaleneTriangleArea(sideA, sideB, sideC):
+    return print("ไม่มีอะไร",sideA, sideB, sideC)
+
+# พื้นที่รูปสี่เหลี่ยมจตุรัศ
+def SquareArea(side):
+    return print("ไม่มีอะไร",side)
+
+# พื้นที่สี่เหลี่ยมผืนผ้า
+def RectangleArea(length, width):
+    return print("ไม่มีอะไร",length, width)
+
+# พื้นที่ห้าเหลี่ยม
+def PentagonArea(side):
+    return print("ไม่มีอะไร",side)
+
+# พื้นที่หกเหลี่ยม
+def HexagonArea(side):
+    return print("ไม่มีอะไร",side)
