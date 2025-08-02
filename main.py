27dc2e634512ebe109ec5c01ace6973c7a2c2d67
Phongsakorn PP <128141477+pp300950@users.noreply.github.com>
2025-08-02 07:54:05 +0000

    ฟังชั่นหาพื้นที่เเบบทั่วไปเเละฟังชั่นการคำนวณทั่วไป

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils import greet
-from math_tools import (
+"""from math_tools import (
     CircleArea,
     TriangleArea,
     EquilateralTriangleArea,
@@ -10,28 +10,30 @@ from math_tools import (
     PentagonArea,
     HexagonArea,
     add
-)
+)"""
 
 import matplotlib.pyplot as plt #ใช้วาดกราฟ
 
-side,unit = 0,"เมตร"
-#ฟังชั่นตรวจสอบการป้อนค่ากากๆ
+side,unit = 0,"เมตร" #ค่าเริ่มต้น
+
+#ฟังชั่นตรวจสอบการป้อนค่า
+array_units = [
+    [['มิลลิเมตร'],[0.001]],  # 1 มม. = 0.001 เมตร
+    [['เซนติเมตร'],[0.01]],   # 1 ซม. = 0.01 เมตร
+    [['นิ้ว'],[0.0254]],      # 1 นิ้ว = 0.0254 เมตร
+    [['เมตร'],[1.0]],    # 1 เมตร = 1 เมตร
+    [['วา'],[2.0]],   # 1 วา = 2 เมตร
+    [['กิโลเมตร'],[1000.0]] # 1 กม. = 1000 เมตร
+]
+
 def validate_input(side, unit):
     array_units_text = [] #เก็บชื่อหน่วยที่ใช้ได้
-    array_units = [
-        'มิลลิเมตร',
-        'เซนติเมตร',
-        'นิ้ว',
-        'เมตร',
-        'วา',
-        'งาน',
-        'ไร่',
-        'กิโลเมตร'
-    ]
     
-    # ตรวจว่า value เป็นตัวเลข จำนวนเต็ม
-    if side != int(side):
+    #ตรวจว่า side เป็นตัวเลข จำนวนเต็ม
+    if side != int(side) and side != float(side):
         return False, "ค่าที่ป้อนไม่ใช่ตัวเลข"
+    elif side == float(side):
+        return False, "ค่าที่ป้อนต้องเป็นจำนวนเต็ม"
 
     #ตรวจว่าค่ามุมไม่ติดลบ
     if side < 0:
@@ -44,20 +46,58 @@ def validate_input(side, unit):
         return False, f"หน่วยไม่ถูกต้อง: {unit} (ใช้ได้เเค่ {', '.join(array_units)})"
     return True, f"รับข้อมูลแล้ว: จน.มุม {side}, หน่วย {unit}"
 
-is_valid, message = validate_input(side, unit)
+# validate_input(-5, 'กิโลเมตร')
+# is_valid, message = validate_input(side, unit)
+# print(message)
 
-
-validate_input(-5, 'กิโลเมตร')
-
-print(message)
 #วิธีการใช้งาน + เปิดโปรเเกรม
-print("ยินดีต้อนรับสู่โปรเเกรมหาพื้นที่")
+print("ยินดีต้อนรับสู่โปรเเกรมหาพื้นที่\nหาพื้นที่เเบบไหนดี?\n1. พิกัดจุด/2. ปกติ")
 
-#รับหน่วยที่ใช้ ตาราง เมตร,ซม,นิ้ว,กิโลเมตร,วา,ไร่,งาน
+while True:
+    try:
+        type_calculator = int(input("ป้อนเลขตัวเลือก: "))
+        if type_calculator == 1:
+            print("คุณเลือกการคำนวณแบบ พิกัดจุด")
+            break
+        elif type_calculator == 2:
+            print("คุณเลือกการคำนวณแบบ ทั่วไป")
+            break
+        else:
+            print("โปรดป้อนเป็นตัวเลข 1 หรือ 2 เท่านั้น")
+    except ValueError:
+        print("กรุณาป้อนเป็นตัวเลข ไม่ใช่ตัวอักษรหรือสัญลักษณ์")
 
-#เเปลงหน่วยไปเป็นเมตรทั้งหมด
 
-#รับจำนวนด้าน
-Side_many = input("ป้อนจำนวนด้าน:")
+# ใช้หาพื้นที่เเบบทั่วไป
+def basic_area():
+    print("เลือกหน่วยของคุณ")
+    for i in range(len(array_units)):
+        if i < (len(array_units)-1):
+            print(array_units[i][0][0],",",end='')
+        else:
+            print(array_units[i][0][0])
 
+    #รับหน่วยที่ใช้ ตาราง เมตร,ซม,นิ้ว,กิโลเมตร,วา,ไร่,งาน
+    #unit_input = input("ป้อนหน่วยของคุณ: ")
+    unit_input = "เซนติเมตร"
+    #เเปลงหน่วยไปเป็นเมตรทั้งหมด 
+    for i in range(len(array_units)):
+        if unit_input == array_units[i][0][0]:
+            real_unit = array_units[i][1][0]
+    #print("ค่าจริงของมันคือ ",real_unit)
+    #รับจำนวนด้าน
+    side_many = input("ป้อนจำนวนด้าน:")
 
+    # ฟังชั่นระบุรูปทรง
+    def what_shape(side_many):
+        if side_many == 0:
+            print("วงกลม")
+        elif side_many == 3:
+            print("สามเหลี่ยม")
+        elif side_many == 4:
+            print("สี่เหลี่ยม")
+        elif side_many == 5:
+            print("ห้าเหลี่ยม")
+        elif side_many == 6:
+            print("หกเหลี่ยม")
+        return f'คุณต้องการหาพื้นที่รูปทรง:'
--- math_tools.py
+++ math_tools.py
@@ -1,3 +1,5 @@
+import math
+
 def add(a, b):
     return a + b
 
@@ -9,36 +11,56 @@ def subtract(a, b):
 
 #พื้นที่วงกลม
 def CircleArea(radius):
-    return print("ไม่มีอะไร",radius)
+    area = 3.14 * (radius**2)
+    return print("พท.วงกลมนะ = ",area)
+CircleArea(5)
 
 #พื้นที่สามเหลี่ยมทั่วไป
 def TriangleArea(base, height):
-    return print("ไม่มีอะไร",base, height)
+    area = 1/2 *base*height
+    return print("พท.สามเหลี่ยมทั่วไป =",area)
+TriangleArea(4,5)
 
+# คำสั่งสะเเควรูท ==> math.sqrt(3)  
 ## พื้นที่สามเหลี่ยมด้านเท่า
 def EquilateralTriangleArea(side):
-    return print("ไม่มีอะไร",side)
+    area = math.sqrt(3)/4*(side)**2
+    return print("พท.สามเหลี่ยมด้านเท่า =",area)
+EquilateralTriangleArea(6)
 
-## พื้นที่สามเหลี่ยมน่าจั่ว 
-def IsoscelesTriangleArea(base, equalSide):
-    return print("ไม่มีอะไร",base, equalSide)
+## พื้นที่สามเหลี่ยมหน้าจั่ว 
+def IsoscelesTriangleArea(base,hieght):
+    area = 1/2*base*hieght
+    return print("พท.สามเหลี่ยมหน้าจั่ว=",area)
+IsoscelesTriangleArea(6,3)
 
 ## พื้นที่สามเหลี่ยมด้านไม่เท่า
 def ScaleneTriangleArea(sideA, sideB, sideC):
+    
     return print("ไม่มีอะไร",sideA, sideB, sideC)
+
 
 # พื้นที่รูปสี่เหลี่ยมจตุรัศ
 def SquareArea(side):
-    return print("ไม่มีอะไร",side)
+    area = side*side
+    return print("พท.รูปสี่เหลี่ยมจตุรัศ=",area)
+SquareArea(4)
 
 # พื้นที่สี่เหลี่ยมผืนผ้า
-def RectangleArea(length, width):
-    return print("ไม่มีอะไร",length, width)
+def RectangleArea(length,width):
+    area = length*width
+    return print("พื้นที่สี่เหลี่ยมผืนผ้า=",area)
+RectangleArea(3,6)
 
 # พื้นที่ห้าเหลี่ยม
-def PentagonArea(side):
-    return print("ไม่มีอะไร",side)
+# perimeter = ความยาวด้าน
+def PentagonArea(perimeter, apothem):
+    area = 1/2*perimeter*apothem
+    return print("พท.ห้าเหลี่ยม=",area)
+PentagonArea(4,4)
 
 # พื้นที่หกเหลี่ยม
-def HexagonArea(side):
-    return print("ไม่มีอะไร",side)
+def HexagonArea(length):
+    area = (3*math.sqrt(3)*(length**2))/2
+    return print("พท.หกเหลี่ยม=",area)
+HexagonArea(6)
